b6e85d94645a530ffaf21efc417855cde485549a
jamesmissett <jamesfm05@gmail.com>
2025-02-01 16:59:34 -0500

    s

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import image1 from './assets/Ambulance.jpg';
+import image2 from './assets/Trump.avif';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+      <div className="images-container">
+          <img src={image1} alt="Image 1" className="header-image" />
+          <img src={image2} alt="Image 2" className="header-image" />
+        </div>
         
       <h1>
       📰 Newsference 📻
