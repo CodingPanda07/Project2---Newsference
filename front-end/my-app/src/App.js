58800b33a1ed315b1f09fba8b2aa5506aaa5df6a
jamesmissett <jamesfm05@gmail.com>
2025-02-01 16:26:57 -0500

    s

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       
+      <h1>
+        Newsference
+      </h1>
         
        
       </header>
